45adf76c7a24cd9321b927bcc2ace4a7f8f37c4a
rafaellarg <rafaellarg@al.insper.edu.br>
2023-05-01 16:51:05 -0300

    mudança2

--- Batalha naval - Código.py
+++ Batalha naval - Código.py
@@ -9,3 +9,25 @@ def define_posicoes(linha,coluna,orientacao,tamanho):
             coluna+=1
 
     return lista
+
+def preenche_frota(frota,nome,linha,coluna,orientacao,tamanho):
+    frota1 = {}
+    def define_posicoes(linha,coluna,orientacao,tamanho):
+        lista = []
+        for i in range(tamanho):
+            if orientacao == "vertical":
+                lista.append([linha,coluna])
+                linha += 1
+            elif orientacao == "horizontal":
+                lista.append([linha,coluna])
+                coluna+=1
+
+        return lista
+
+    frotas = define_posicoes(linha,coluna,orientacao,tamanho)
+
+    for itens in frota:
+        frota1[nome] = itens + frotas
+ 
+    return frota1
+    
