8083920ca6152b2b678ecdffc119bf5a02b3fc64
rafaellarg <rafaellarg@al.insper.edu.br>
2023-05-02 16:39:39 -0300

    mudanca3

--- Batalha naval - Código.py
+++ Batalha naval - Código.py
@@ -25,4 +25,25 @@ def faz_jogada(tabuleiro, linha, coluna):
     else:
         tabuleiro[linha][coluna] = '-'
     return tabuleiro
-    
+
+def posiciona_frota(frota):
+  grid = []
+  linhas = [0]*10
+  for i in linhas:
+    grid.append(([i*10])*10)
+
+
+  for nome in frota:
+    for l in frota[nome][0][0][0]:
+      for c in frota[nome][0][0][1]:
+        if l == l+1:
+          grid[l].append(1[c])
+
+      
+      for c2 in frota[nome][0][0][1]:
+        for l2 in frota[nome][0][0][0]:
+          if c2 == c2+1:
+            grid[c2].append(1[l2])
+
+  return grid
+
